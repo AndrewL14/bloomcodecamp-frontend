df216a048556bd665affc70a222f313e37a20531
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-05-09 03:14:27 -0500

    Modified html and assignment list.
    in the assinment list and HTML added a status.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,10 +5,22 @@ import fetcher from '../assets/utils/fetcherUtils';
 
 function Dashboard(props) {
   const assignments1 = [
-    { id: 1, number: 101, githubUrl: "https://github.com/BloomTechBackend/bd-java-fundamentals-project-TheProgrammer18" },
-    { id: 2, number: 201, githubUrl: "https://github.com/BloomTechBackend/bd-futures-found-me-AndrewL14" },
-    { id: 3, number: 301, githubUrl: "https://github.com/BloomTechBackend/bd-in-memory-caching-amazon-gaming-membership-AndrewL14" },
-    { id: 4, number: 401, githubUrl: "https://github.com/BloomTechBackend/bd-intro-to-hardware-and-operating-systems-AndrewL14" }
+    { id: 1, 
+      number: 101, 
+      stat: "pending",
+      githubUrl: "https://github.com/BloomTechBackend/bd-java-fundamentals-project-TheProgrammer18"},
+    { id: 2, 
+      number: 201, 
+      stat: "completed",
+      githubUrl: "https://github.com/BloomTechBackend/bd-futures-found-me-AndrewL14" },
+    { id: 3, 
+      number: 301, 
+      stat: "completed",
+      githubUrl: "https://github.com/BloomTechBackend/bd-in-memory-caching-amazon-gaming-membership-AndrewL14" },
+    { id: 4, 
+      number: 401, 
+      stat: "pending",
+      githubUrl: "https://github.com/BloomTechBackend/bd-intro-to-hardware-and-operating-systems-AndrewL14" }
   ];
 
   const navigate = useNavigate();
@@ -42,6 +54,7 @@ function Dashboard(props) {
             return (
               <div className='assignmentCard' key={item.id}>
                 <h2> Assignment #{item.number}</h2>
+                <h5> Status: {item.stat}</h5>
                 <p>Github URL: {item.githubUrl}</p>
                 <button className="edit-btn">Edit</button>
               </div>
@@ -49,6 +62,10 @@ function Dashboard(props) {
           })
         }
       </div>
+      <button className="logout-button" onClick={() => {window.location.href = "http://localhost:3000/"}}>
+        <span>Logout</span>
+        <i></i>
+      </button>
     </div>
   )
 }
