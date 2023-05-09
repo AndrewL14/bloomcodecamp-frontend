76b7b4ec9c576bf909cbf0d97738443fa4859e85
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-05-09 03:17:19 -0500

    added a cloud/modfied login button.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,9 +3,15 @@ import React from 'react';
 function HomePage(props) {
   return (
     <div>
-      <h1 className="heading">HOMEPAGE</h1>
+      <div className='container'>
+        <div className='cloud' />
+      </div>
+      <h1 className="heading">Homepage</h1>
       <h3 className="subheading">Welcome Learners and CodeReviewers!</h3>
-      <button className="login-button" onClick={() => {window.location.href = "http://localhost:3000/login"}}>Login</button>
+      <button className="homepage-button" onClick={() => {window.location.href = "http://localhost:3000/login"}}>
+        <span>Login</span>
+        <i></i>
+      </button>
     </div>
   );
 }
